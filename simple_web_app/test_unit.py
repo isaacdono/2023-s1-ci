3161f70f15e98828c283ad35f856f159d30ccec5
Isaac <i247175@dac.unicamp.br>
2024-09-12 09:24:06 -0300

    fix: correct number of blank lines

--- simple_web_app/test_unit.py
+++ simple_web_app/test_unit.py
@@ -60,12 +60,14 @@ def test_8_chars_with_no_especial_character_password_should_raise_exception():
         # when
         validator.password_validate()
 
+
 def test_8_chars_with_slash_character_password_should_raise_exception():
     # given
     validator = PasswordValidator(content="1eAB1eAB")
     with pytest.raises(Exception):  # then
         # when
         validator.password_validate()
+
 
 def test_8_chars_with_caret_character_password_should_raise_exception():
     # given
@@ -74,12 +76,14 @@ def test_8_chars_with_caret_character_password_should_raise_exception():
         # when
         validator.password_validate()
 
+
 def test_8_chars_with_tilde_character_password_should_raise_exception():
     # given
     validator = PasswordValidator(content="1eAB1eAB")
     with pytest.raises(Exception):  # then
         # when
         validator.password_validate()
+
 
 def test_8_chars_with_no_upper_case_password_should_raise_exception():
     # given
