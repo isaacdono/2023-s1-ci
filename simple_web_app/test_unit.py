f1ebe1e5083f700c8d7c9c69b22e85949e794698
Isaac <i247175@dac.unicamp.br>
2024-09-12 09:20:20 -0300

    fix: more tests implementation

--- simple_web_app/test_unit.py
+++ simple_web_app/test_unit.py
@@ -60,6 +60,26 @@ def test_8_chars_with_no_especial_character_password_should_raise_exception():
         # when
         validator.password_validate()
 
+def test_8_chars_with_slash_character_password_should_raise_exception():
+    # given
+    validator = PasswordValidator(content="1eAB1eAB")
+    with pytest.raises(Exception):  # then
+        # when
+        validator.password_validate()
+
+def test_8_chars_with_caret_character_password_should_raise_exception():
+    # given
+    validator = PasswordValidator(content="1eAB1eAB")
+    with pytest.raises(Exception):  # then
+        # when
+        validator.password_validate()
+
+def test_8_chars_with_tilde_character_password_should_raise_exception():
+    # given
+    validator = PasswordValidator(content="1eAB1eAB")
+    with pytest.raises(Exception):  # then
+        # when
+        validator.password_validate()
 
 def test_8_chars_with_no_upper_case_password_should_raise_exception():
     # given
